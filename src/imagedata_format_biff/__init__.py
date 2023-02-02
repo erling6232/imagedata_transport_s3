f8f00bd9c8883ccc6f8682105d2098a09676b24e
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2023-02-02 14:52:50 +0100

    Minor updates.

--- src/imagedata_format_biff/__init__.py
+++ src/imagedata_format_biff/__init__.py
@@ -5,15 +5,16 @@ import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 try:
-    from importlib.metadata import version, entry_points
+    from importlib.metadata import version
     __version__ = version('imagedata_format_biff')
 except ModuleNotFoundError:
-    from importlib_metadata import version, entry_points
+    from importlib_metadata import version
     __version__ = version('imagedata_format_biff')
 except Exception:
-    import imagedata as _
+    # import imagedata as _
+    from . import __path__ as _path
     from os.path import join
-    with open(join(_.__path__[0], "..", "VERSION.txt"), 'r') as fh:
+    with open(join(_path[0], "..", "VERSION.txt"), 'r') as fh:
         __version__ = fh.readline().strip()
 
 __author__ = 'Erling Andersen, Haukeland University Hospital, Bergen, Norway'
