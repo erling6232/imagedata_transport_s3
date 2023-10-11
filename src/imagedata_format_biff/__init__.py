4b320ed375127d854611d27ad5a489be277e52f5
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2023-10-11 13:36:08 +0200

    Add biff mimetypes.

--- src/imagedata_format_biff/__init__.py
+++ src/imagedata_format_biff/__init__.py
@@ -1,21 +1,32 @@
 """imagedata_format_biff"""
 
 import logging
+import mimetypes
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 try:
     from importlib.metadata import version
     __version__ = version('imagedata_format_biff')
-except ModuleNotFoundError:
-    from importlib_metadata import version
-    __version__ = version('imagedata_format_biff')
+#except ModuleNotFoundError:
+#    from importlib_metadata import version
+#    __version__ = version('imagedata_format_biff')
 except Exception:
     # import imagedata as _
     from . import __path__ as _path
-    from os.path import join
-    with open(join(_path[0], "..", "VERSION.txt"), 'r') as fh:
-        __version__ = fh.readline().strip()
+    from os.path import isfile, join
+    _fname = join(_path[0], "..", "VERSION.txt")
+    if not isfile(_fname):
+        _fname = join(_path[0], "..", "..", "VERSION.txt")
+    if isfile(_fname):
+        with open(_fname, 'r') as fh:
+            __version__ = fh.readline().strip()
+    else:
+        __version__ = None
 
 __author__ = 'Erling Andersen, Haukeland University Hospital, Bergen, Norway'
 __email__ = 'Erling.Andersen@Helse-Bergen.NO'
+
+mimetypes.add_type('application/biff', '.biff')
+mimetypes.add_type('application/biff', '.us')
+mimetypes.add_type('application/biff', '.real')
