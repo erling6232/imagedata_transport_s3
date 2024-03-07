625c6c16977ca91436cc36db999d33019d943b43
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2024-03-07 10:44:42 +0100

    Initial release.

--- src/imagedata_transport_s3/s3transport.py
+++ src/imagedata_transport_s3/s3transport.py
@@ -117,6 +117,7 @@ class S3Transport(AbstractTransport):
         """
         def _yield_dir(parent_dir):
             logger.debug('_yield_dir: {}'.format(parent_dir))
+            yield '/', [] []
             # yield parent_dir, parent_dir['dirs'], parent_dir['files']
             # for d in parent_dir['dirs'].keys():
             #     logger.debug('_yield_dir: key {}'.format(d))
