590239650cb1fbeb44e49bf5e5805ded46f41f1b
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2026-05-29 07:16:47 +0200

    Updated

--- src/imagedata_transport_s3/__init__.py
+++ src/imagedata_transport_s3/__init__.py
@@ -8,16 +8,7 @@ try:
     from importlib.metadata import version
     __version__ = version('imagedata_transport_s3')
 except Exception:
-    from . import __path__ as _path
-    from os.path import isfile, join
-    _fname = join(_path[0], "..", "VERSION.txt")
-    if not isfile(_fname):
-        _fname = join(_path[0], "..", "..", "VERSION.txt")
-    if isfile(_fname):
-        with open(_fname, 'r') as fh:
-            __version__ = fh.readline().strip()
-    else:
-        __version__ = None
+    __version__ = None
 
 __author__ = 'Erling Andersen, Haukeland University Hospital, Bergen, Norway'
 __email__ = 'Erling.Andersen@Helse-Bergen.NO'
